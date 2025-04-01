52125db0cafdbfc64d98d48979fd19526ca2ce01
CaioHGuedes <caiocicm@gmail.com>
2025-03-31 23:32:23 -0300

    final da navegacao prev e next

--- js/slide.js
+++ js/slide.js
@@ -9,6 +9,10 @@ export default class Slide {
     };
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? "transform .3s" : "";
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -31,6 +35,7 @@ export default class Slide {
     }
 
     this.wrapper.addEventListener(moveType, this.onMove);
+    this.transition(false);
   }
 
   onMove(event) {
@@ -46,6 +51,19 @@ export default class Slide {
     const moveType = event.type === "mouseup" ? "mousemove" : "touchmove";
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
+    console.log(this.dist.movement);
   }
 
   addSlideEvents() {
@@ -88,17 +106,26 @@ export default class Slide {
     };
   }
 
-  chengeSlide(index) {
+  changeSlide(index) {
     const activeSlide = this.slideArray[index];
     this.moveSlide(activeSlide.position);
     this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide.position;
   }
 
+  activePrevSlide() {
+    if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) this.changeSlide(this.index.next);
+  }
+
   init() {
-    this.slidesConfig();
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
+    this.slidesConfig();
     return this;
   }
 }
